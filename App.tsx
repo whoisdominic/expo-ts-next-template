8d8301f4a95da45fa6eee6eb1368ebb7d34a74bd
Dominic Cobb <dominicshajaa@icloud.com>
2020-10-26 19:45:34 -0700

    template created

--- App.tsx
+++ App.tsx
@@ -1,19 +1,19 @@
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import 'react-native-gesture-handler';
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import "react-native-gesture-handler";
 
-import RootStack from "./screens/stacks/RootStack";
-import MainStack from "./screens/stacks/MainStack"
+import RootStack from "./mobile/stacks/RootStack";
+import MainStack from "./mobile/stacks/MainStack";
 
 const LandingStack = createStackNavigator();
 
-const auth: boolean = false
+const auth: boolean = true;
 
 export default function App() {
   return (
     <NavigationContainer>
-      {auth? <MainStack /> : <RootStack />} 
+      {auth ? <MainStack /> : <RootStack />}
     </NavigationContainer>
-    )
+  );
 }
